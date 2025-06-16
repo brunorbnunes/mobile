0ba6aa174bb66aa8ba8962a96c2b0f561772410b
brunorbnunes <brunolcbjlf@gmail.com>
2025-06-16 17:02:46 -0300

    backend

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,12 @@ async function initDB() {
         created_at DATE NOT NULL DEFAULT CURRENT_DATE
         )`;
 
+        //DECIMAL (10,2)
+        //Means: a fixed-point number with:
+        // 10 digits in total 
+        // 2 digits after the decimal point
+        // so: the max value is 99999999.99 (8 digits, before the decimal point, and 2 digits after)
+
         console.log("Database initialize successfully");
     } catch (error) {
         console.log("Error initializing database:", error);
@@ -29,10 +35,23 @@ async function initDB() {
 
 //connectDB(process.env.DATABASE_URL)
 
-app.get("/",(req,res) => {
-    res.send("It's working")
-});
+//app.get("/",(req,res) => {
+//    res.send("It's working")
+//});
 
+app.post("/api/transactions", async (req, res) => {
+    const { user_id, title, amount, category } = req.body;
+
+    try {
+        const result = await sql`INSERT INTO transactions (user_id, title, amount, category)
+        VALUES (${user_id}, ${title}, ${amount}, ${category}) RETURNING *`;
+
+        res.status(201).json(result);
+    } catch (error) {
+        console.error("Error creating transaction:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+});
 
 //app.listen(PORT, () => {
 //    console.log("Server is running on PORT:", PORT );
